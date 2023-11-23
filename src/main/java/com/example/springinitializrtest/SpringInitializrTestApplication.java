a06acb6262ae3e5db06076e421a5631dd1ae21dc
Ivan Nikolov <ivan.nikolov@cleverpine.com>
2023-11-23 14:00:29 +0200

    Initial commit

---
+++ src/main/java/com/example/springinitializrtest/SpringInitializrTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinitializrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitializrTestApplication.class, args);
+    }
+
+}
